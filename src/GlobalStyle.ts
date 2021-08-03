cce01a174be257f9544821b46600b648ca7d0d05
SmileyDev <nemanja.gakovic@hotmail.com>
2021-08-03 11:31:43 +0200

    Initial commit

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+
+* {
+      box-sizing: border-box;
+      font-family: 'Abel', sans-serif;
+}
+
+body {
+      margin: 0;
+      padding: 0;
+
+}
+`;
+
+export default GlobalStyle;
